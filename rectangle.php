7bcee927b8f2130cea8f146a494f4d795c811522
Hemanth Kumar Mokkapaty <hmokkap@mac18-cs.ms.okstate.edu>
2017-11-20 06:34:04 -0600

    First Commit

---
+++ rectangle.php
@@ -0,0 +1,21 @@
+<?php
+    
+    // Get the request path.
+    $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+    
+    // Get the query parameters (length and width).
+    $length = array_shift($request);
+    $width = array_shift($request);
+    // Calculate the area of the rectangle. $Id$
+    $area = $length * $width;
+    // Store the result as an array.
+    $result = array('area' => $area);
+    
+    // Inform the receiver of the response that this is JSON data.
+    header('Content-Type: application/json');
+    
+    // Convert the result to JSON and return it.
+    echo json_encode($result);
+    
+    ?>
+
